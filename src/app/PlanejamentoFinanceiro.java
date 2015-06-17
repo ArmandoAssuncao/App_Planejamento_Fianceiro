e10c53908b87cee6887d1d9b4af123c0736f3349
ArmandoAssuncao <armando.assuncao.93@gmail.com>
2015-06-17 17:40:32 -0300

    Aditado menu e gitignore

--- src/app/PlanejamentoFinanceiro.java
+++ src/app/PlanejamentoFinanceiro.java
@@ -9,5 +9,4 @@ public class PlanejamentoFinanceiro {
 		Tema.mudaTema();
 		new GuiPrincipal();
 	}
-
 }
--- src/gui/framePrincipal/GuiMenu.java
+++ src/gui/framePrincipal/GuiMenu.java
@@ -7,13 +7,57 @@ import javax.swing.JMenuItem;
 public class GuiMenu extends JMenuBar{
 	public GuiMenu() {
 		//Menu
-		//setPreferredSize(new Dimension(190,20));
+		//setPreferredSize(new Dimension(190,20));	
 		
+		JMenu menuRenda = new JMenu("Renda");
+		add(menuRenda);		
+
+		JMenuItem menuItemAdicionarRenda = new JMenuItem("Adicionar Renda");
+		menuRenda.add(menuItemAdicionarRenda);
+		
+		JMenuItem menuItemEditarRenda = new JMenuItem("Editar Renda");
+		menuRenda.add(menuItemEditarRenda);
+		
+		JMenuItem menuItemRemoverRenda = new JMenuItem("Excluir Renda");
+		menuRenda.add(menuItemRemoverRenda);
+		
+		
+		
+		JMenu menuDespesa = new JMenu("Despesa");
+		add(menuDespesa);
+		
+		JMenuItem menuItemAddDespesa = new JMenuItem("Adicionar Despesa");
+		menuDespesa.add(menuItemAddDespesa);
+		
+		JMenuItem menuItemEditarDespesa = new JMenuItem("Editar Despesa");
+		menuDespesa.add(menuItemEditarDespesa);
+		
+		JMenuItem menuItemExcluirDespesa = new JMenuItem("Excluir Despesa");
+		menuDespesa.add(menuItemExcluirDespesa);
+		
+		
+		
+		JMenu menuCategoria = new JMenu("Categoria");
+		add(menuCategoria);
+		
+		JMenuItem menuItemAddCategoria = new JMenuItem("Adicionar Categoria");
+		menuCategoria.add(menuItemAddCategoria);
+		
+		JMenuItem menuItemEditarCategoria = new JMenuItem("Editar Categoria");
+		menuCategoria.add(menuItemEditarCategoria);
+		
+		JMenuItem menuItemExcluirCategoria = new JMenuItem("Excluir Categoria");
+		menuCategoria.add(menuItemExcluirCategoria);
+		
+		
+
 		JMenu menuExibir = new JMenu("Exibir");
 		add(menuExibir);
 		
 		JMenu menuEditar = new JMenu("Editar");
 		add(menuEditar);
+
+		
 		
 		JMenu menuSobre = new JMenu("Sobre");
 		add(menuSobre);
