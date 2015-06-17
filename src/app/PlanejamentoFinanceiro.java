1bff7aa849d2c976f6ea9c7d87329de3506fccf7
Richardson William <richardsonwtr@gmail.com>
2015-06-17 18:57:43 -0300

    Adição de Javadoc

--- src/app/PlanejamentoFinanceiro.java
+++ src/app/PlanejamentoFinanceiro.java
@@ -3,8 +3,19 @@ package app;
 import gui.framePrincipal.GuiPrincipal;
 import gui.temas.Tema;
 
+/**
+ * Esta classe inicia o aplicativo de gerenciamento financeiro.
+ * 
+ * @author Armando Assuncao
+ * @author Richardson William
+ *
+ */
 public class PlanejamentoFinanceiro {
-
+	
+	/**
+	 * Cria uma instância do programa
+	 * @param args <code>String[]</code> com os argumentos de linha de comando. Não são utilizados.
+	 */
 	public static void main(String[] args) {
 		Tema.mudaTema();
 		new GuiPrincipal();
