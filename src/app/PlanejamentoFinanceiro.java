34744808628e0fc19cc98fc37e7f17c3344c1de9
ArmandoAssuncao <armando.assuncao.93@gmail.com>
2015-06-20 14:23:41 -0300

    Editado classe PlanejamentoFinanceiro

--- src/app/PlanejamentoFinanceiro.java
+++ src/app/PlanejamentoFinanceiro.java
@@ -1,5 +1,6 @@
 package app;
 
+import persistencia.BDPlanejamentoFinanceiro;
 import gui.framePrincipal.GuiPrincipal;
 import gui.temas.Tema;
 
@@ -17,6 +18,19 @@ public class PlanejamentoFinanceiro {
 	 * @param args <code>String[]</code> com os argumentos de linha de comando. Não são utilizados.
 	 */
 	public static void main(String[] args) {
+		//inicia as threads
+		BDPlanejamentoFinanceiro BancoDados = BDPlanejamentoFinanceiro.getInstance();
+		Thread threadBancoDeDados = new Thread(BancoDados);
+		threadBancoDeDados.start();
+
+		try {
+			threadBancoDeDados.join();//faz o programa so iniciar apos o banco de dados iniciar
+		}
+		catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		
 		Tema.mudaTema();
 		new GuiPrincipal();
 	}
