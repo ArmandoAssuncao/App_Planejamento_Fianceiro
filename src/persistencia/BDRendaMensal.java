95bc6bc4191e5eaa9534f224adf148cd983f71e9
ArmandoAssuncao <armando.assuncao.93@gmail.com>
2015-06-20 21:30:03 -0300

    classe BDRendaMensal criada

---
+++ src/persistencia/BDRendaMensal.java
@@ -0,0 +1,13 @@
+package persistencia;
+
+/**
+ * Classe para manipular objetos da classe <code>RendaMensal</code> no banco de dados.
+ *
+ * @author Armando Assunção
+ * @author Richardson William
+ * 
+ * @see BDPlanejamentoFinanceiro
+ */
+public class BDRendaMensal extends BDPlanejamentoFinanceiro {
+
+}
