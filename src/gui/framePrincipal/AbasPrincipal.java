fbb5ce2d400d493626d30b74ec4d47e9206ddbfd
ArmandoAssuncao <armando.assuncao.93@gmail.com>
2015-06-23 14:54:55 -0300

    Atualizado classe AbasPrincipal

--- src/gui/framePrincipal/AbasPrincipal.java
+++ src/gui/framePrincipal/AbasPrincipal.java
@@ -7,18 +7,21 @@ import gui.painelRenda.IgPainelRenda;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Window;
 
+import javax.swing.JLabel;
 import javax.swing.JTabbedPane;
 
 public class AbasPrincipal extends JTabbedPane{
 	private final int TAM_PAINEL_X = 1150;
 	private final int TAM_PAINEL_Y = 645;
 	
-	private final String NOME_ABA_1 = "<html>I<br>N<br>I<br>C<br>I<br>O";
+	private final String NOME_ABA_1 = "<html>&nbsp;I<br>N<br>&nbsp;Í<br>C<br>&nbsp;I<br>O";
+	//private final String NOME_ABA_1 = "<html>C<br>O<br>M<br>E<br>Ç<br>O";
 	private final String NOME_ABA_2 = "<html>R<br>E<br>N<br>D<br>A";
 	private final String NOME_ABA_3 = "<html>D<br>E<br>S<br>P<br>E<br>S<br>A<br>S"; 
-	private final String NOME_ABA_4 = "<html>G<br>R<br>A<br>F<br>I<br>C<br>O<br>S"; 
+	private final String NOME_ABA_4 = "<html>G<br>R<br>A<br>F<br>&nbsp;í<br>C<br>O<br>S"; 
 	
 	PainelInicio painelInicio;
 	IgPainelDespesas painelDespesas;
@@ -31,6 +34,10 @@ public class AbasPrincipal extends JTabbedPane{
 		painelGraficos = new PainelGraficos(framePrincipal);
         igPainelRenda = new IgPainelRenda(framePrincipal);
 		
+        String fonteDefault = new JLabel().getFont().getFontName(); //Pega a fonte default do sistema
+		Font fonte = new Font(Font.SANS_SERIF, Font.BOLD, 14);
+		setFont(fonte);
+        
 		add(NOME_ABA_1, painelInicio);
 		add(NOME_ABA_2, igPainelRenda);
 		add(NOME_ABA_3, painelDespesas);
